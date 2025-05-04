569562fff786aefadb2a97b6d7459b1abca8a11d
Vagner-Alves <vagneralves997@gmail.com>
2025-05-04 14:58:47 -0300

    agendamento repository

---
+++ src/main/java/ifpe/edu/br/nexus_saude/repository/AgendamentoRepository.java
@@ -0,0 +1,7 @@
+package ifpe.edu.br.nexus_saude.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import ifpe.edu.br.nexus_saude.model.Agendamento;
+
+public interface AgendamentoRepository extends JpaRepository<Agendamento, Integer>{
+
+}
