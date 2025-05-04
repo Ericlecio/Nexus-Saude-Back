599dce33f52209941ba8964573808956620464a0
Vagner-Alves <vagneralves997@gmail.com>
2025-05-04 14:26:54 -0300

    consulta entidade

---
+++ src/main/java/ifpe/edu/br/nexus_saude/model/Consulta.java
@@ -0,0 +1,48 @@
+package ifpe.edu.br.nexus_saude.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "consultas")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Consulta {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "consulta_id")
+	private Integer consultaId;
+
+	@ManyToOne
+	private Paciente paciente;
+
+	@ManyToOne
+	private Medico medico;
+
+	@Column(nullable = false)
+	private String data;
+
+	@Column(nullable = false)
+	private String especialidade;
+
+	@Column(nullable = false)
+	private String local;
+
+	@ManyToOne
+	private SituacaoAgendamento situacao;
+
+	@Column(nullable = false)
+	private String createdAt;
+
+	@Column(nullable = false)
+	private String updatedAt;
+}
