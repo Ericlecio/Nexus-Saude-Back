b31aaca10388b484287ad853b0b44d35ac899f7a
Vagner-Alves <vagneralves997@gmail.com>
2025-05-04 21:57:57 -0300

    apagando entidade usuario

--- src/main/java/ifpe/edu/br/nexus_saude/model/Consulta.java
+++ src/main/java/ifpe/edu/br/nexus_saude/model/Consulta.java
@@ -1,5 +1,8 @@
 package ifpe.edu.br.nexus_saude.model;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,11 +12,14 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "consultas")
 @Data
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Consulta {
