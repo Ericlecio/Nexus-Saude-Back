c9dda0137c74dbd2887d5709f13a3ba6636788c0
Vagner-Alves <vagneralves997@gmail.com>
2025-05-25 23:53:33 -0300

    fix: paciente

--- src/main/java/ifpe/edu/br/nexus_saude/model/Paciente.java
+++ src/main/java/ifpe/edu/br/nexus_saude/model/Paciente.java
@@ -17,11 +17,15 @@ public class Paciente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer pacienteId;
+    
+    @OneToOne(cascade = CascadeType.ALL, optional = false, orphanRemoval = true)
+    @JoinColumn(name = "usuario_id", referencedColumnName = "id", unique = true)
+    private Usuario usuario;
+    
     @Column(length = 100, nullable = false)
     private String nomeCompleto;
 
-    @Column(length = 50, nullable = false, unique = true)
-    private String email;
+   // email e senha estão na class usuario
 
     @Column(length = 15, nullable = false)
     private String telefone;
@@ -43,7 +47,7 @@ public class Paciente {
     @PrePersist
     protected void onCreate() {
         this.createdAt = LocalDateTime.now();
-        this.dataCadastro = LocalDateTime.now();
+        this.dataCadastro = LocalDateTime.now(); // dataCadastro já existe e é setado aqui
     }
 
     @PreUpdate
@@ -53,7 +57,7 @@ public class Paciente {
 
     @Override
     public String toString() {
-        return "Paciente [pacienteId=" + pacienteId + ", nomeCompleto=" + nomeCompleto + ", email=" + email +
+        return "Paciente [pacienteId=" + pacienteId + ", nomeCompleto=" + nomeCompleto + (usuario != null ? ", email=" + usuario.getEmail() : "") +
                 ", cpf=" + cpf + ", planoSaude=" + planoSaude + "]";
     }
 }
---
+++ src/main/java/ifpe/edu/br/nexus_saude/repository/UsuarioRepository.java
@@ -0,0 +1,5 @@
+package ifpe.edu.br.nexus_saude.repository;
+
+public interface UsuarioRepository {
+
+}
