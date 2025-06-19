b4921873ba571661bba0010dde0bd8a93527539f
Vagner-Alves <vagneralves997@gmail.com>
2025-06-19 00:57:05 -0300

    create: validador jwt e chave secreta

---
+++ src/main/java/ifpe/edu/br/nexus_saude/security/JwtTokenProvider.java
@@ -0,0 +1,57 @@
+package ifpe.edu.br.nexus_saude.security;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+	@Value("${app.jwtSecret}")
+	private String jwtSecret;
+
+	@Value("${app.jwtExpirationInMs}")
+	private int jwtExpirationInMs;
+
+	private SecretKey getSigningKey() {
+		return Keys.hmacShaKeyFor(jwtSecret.getBytes());
+	}
+
+	public String generateToken(Authentication authentication) {
+		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+		Date now = new Date();
+		Date expiryDate = new Date(now.getTime() + jwtExpirationInMs);
+
+		return Jwts.builder()
+				.subject(userDetails.getUsername())
+				.issuedAt(new Date())
+				.expiration(expiryDate)
+				.signWith(getSigningKey())
+				.compact();
+	}
+
+	public String getUsernameFromJWT(String token) {
+		Claims claims = Jwts.parser()
+				.verifyWith(getSigningKey())
+				.build()
+				.parseSignedClaims(token)
+				.getPayload();
+
+		return claims.getSubject();
+	}
+
+	public boolean validateToken(String authToken) {
+		try {
+			Jwts.parser().verifyWith(getSigningKey()).build().parseSignedClaims(authToken);
+			return true;
+		} catch (Exception ex) {
+			// MalformedJwtException, ExpiredJwtException, UnsupportedJwtException, IllegalArgumentException
+		}
+		return false;
+	}
+}
